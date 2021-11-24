82eb3c0deb3e1ef10f90afbb38578552afcf0904
laihaojie <53169820+laihaojie@users.noreply.github.com>
2021-11-24 23:58:34 +0800

    基本架构搭建完成

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import path from 'path'
+import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 import { exists, copyDir } from "./utils/copy.js"
 import questions from "./questions/index.js";
@@ -8,13 +9,15 @@ import relation from "./utils/constant.js"
 
 
 const answer = await questions();
-console.log(answer);
 
 const __dirname = fileURLToPath(
     import.meta.url)
+console.log(chalk.blue(`创建${answer.project}项目`))
 // console.log(getRootPath());
 // console.log(path.resolve(__dirname, "../template/html/"));
 exists(path.resolve(__dirname, relation[answer.project]), getRootPath(), copyDir)
+
+console.log(chalk.blue(`创建完成`))
 
 function getRootPath() {
     return process.cwd()
