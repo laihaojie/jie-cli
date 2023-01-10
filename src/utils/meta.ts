2e2f0815c07d2da4d014739eb80e7d4573e069d5
阿杰 <53169820+laihaojie@users.noreply.github.com>
2023-01-10 09:39:08 +0800

    feat: add create project

--- src/utils/meta.ts
+++ src/utils/meta.ts
@@ -2,4 +2,5 @@ export default {
   Vue3: 'npx degit antfu/vitesse',
   nestjs: 'npx degit laihaojie/starter-nestjs#main',
   html: 'npx degit laihaojie/starter-html#main',
+  electron: 'npx degit https://github.com/Deluze/electron-vue-template',
 }
