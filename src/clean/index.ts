3d391c907ad41c4f2927797908e462166400e14b
阿杰 <53169820+laihaojie@users.noreply.github.com>
2023-05-30 18:26:18 +0800

    refactor: update

--- src/clean/index.ts
+++ src/clean/index.ts
@@ -3,7 +3,7 @@ import { rimraf } from 'rimraf'
 import chalk from 'chalk'
 import ora from 'ora'
 
-export function clean(args: string[]) {
+export function clean(args: string[]): any {
   if (args.length) return cleanByPath(...args)
 
   const cleans = [
@@ -22,25 +22,35 @@ export function clean(args: string[]) {
   cleanByPath(...cleans)
 }
 
-function cleanByPath(...args: string[]) {
+// function cleanByPath(...args: string[]) {
+//   const spinner = ora()
+
+//   next(args[Symbol.iterator]())
+
+//   async function next(iterator) {
+//     const result = iterator.next()
+
+//     if (result.done) return
+
+//     const value = result.value
+
+//     if (!fs.existsSync(value)) return
+
+//     spinner.start(chalk(`清除 ${chalk.bold(value)}`))
+//     await rimraf(value)
+//     spinner.succeed(chalk.green(`清除 ${chalk.bold(value)}`))
+//     next(iterator)
+//   }
+// }
+async function cleanByPath(...args: string[]) {
   const spinner = ora()
 
-  next(args[Symbol.iterator]())
-
-  async function next(iterator) {
-    const result = iterator.next()
-
-    if (result.done) return
-
-    const value = result.value
-
-    if (!fs.existsSync(value)) return
+  for (const value of args) {
+    if (!fs.existsSync(value)) continue
 
     spinner.start(chalk(`清除 ${chalk.bold(value)}`))
     await rimraf(value)
     spinner.succeed(chalk.green(`清除 ${chalk.bold(value)}`))
-    next(iterator)
   }
 }
-
 
