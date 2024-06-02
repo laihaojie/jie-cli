68f6a76081f4699462cffee4c8514787763238de
阿杰 <53169820+laihaojie@users.noreply.github.com>
2024-06-02 14:58:38 +0800

    chore: release v1.0.44

--- build.config.ts
+++ build.config.ts
@@ -10,4 +10,20 @@ export default defineBuildConfig({
     emitCJS: true,
     inlineDependencies: true,
   },
+  hooks: {
+    'rollup:options': (ctx, options) => {
+      // @ts-expect-error xxx
+      options.plugins.push(
+        {
+          name: 'replace_string_decoder',
+          generateBundle(outputOptions, bundle) {
+            for (const [fileName, chunkOrAsset] of Object.entries<any>(bundle)) {
+              if (['jie.mjs', 'jie.cjs'].includes(fileName))
+                chunkOrAsset.code = chunkOrAsset.code.replaceAll('string_decoder/', 'string_decoder')
+            }
+          },
+        },
+      )
+    },
+  },
 })
